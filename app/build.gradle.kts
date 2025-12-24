54782e7a18e0eff3c0bc3fcdf0093d1be8c0b3a8
Clause Extraction Developer <developer@clauseextraction.com>
2025-12-24 14:58:38 +0530

    Fix Android build errors - XML entities, missing resources, KAPT issues
    - Fixed XML entity errors: escaped ampersands (&) in text attributes
    - Created missing drawable resources: ic_chat_empty, ic_search_empty
    - Fixed fragment_splash.xml premature end of file error
    - Fixed themes.xml style inheritance issues
    - Temporarily disabled KAPT/Hilt due to Java module access issues
    - Updated AndroidManifest to use drawable icons instead of missing mipmaps
    - Added JVM arguments for Java module access (for future KAPT fix)
    
    Current status: XML and resource errors fixed, KAPT/Hilt compilation issues remain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
+    // id("kotlin-kapt")
+    // id("dagger.hilt.android.plugin")
     id("kotlin-parcelize")
 }
 
@@ -37,6 +37,9 @@ android {
     
     kotlinOptions {
         jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-Xjvm-default=all"
+        )
     }
     
     buildFeatures {
@@ -62,23 +65,23 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     
-    // Hilt Dependency Injection
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+    // Hilt Dependency Injection - Temporarily disabled due to KAPT issues
+    // implementation("com.google.dagger:hilt-android:2.48")
+    // kapt("com.google.dagger:hilt-compiler:2.48")
     
-    // Room Database
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    // Room Database - Temporarily disabled due to KAPT issues
+    // implementation("androidx.room:room-runtime:2.6.1")
+    // implementation("androidx.room:room-ktx:2.6.1")
+    // kapt("androidx.room:room-compiler:2.6.1")
     
     // Retrofit for API calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     
-    // Image Loading
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    kapt("com.github.bumptech.glide:compiler:4.16.0")
+    // Image Loading - Temporarily disabled due to KAPT issues
+    // implementation("com.github.bumptech.glide:glide:4.16.0")
+    // kapt("com.github.bumptech.glide:compiler:4.16.0")
     
     // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
@@ -102,6 +105,7 @@ dependencies {
     testImplementation("io.kotest:kotest-property:5.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
-    kaptAndroidTest("com.google.dagger:hilt-compiler:2.48")
+    // Testing - Temporarily disabled due to KAPT issues
+    // androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
+    // kaptAndroidTest("com.google.dagger:hilt-compiler:2.48")
 }
